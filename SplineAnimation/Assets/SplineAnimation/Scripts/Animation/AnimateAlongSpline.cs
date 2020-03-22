f2c8a5cdbbf8e75381851af73108d072c67a2d06
Michael <michael.feldman09@gmail.com>
2020-03-22 12:53:48 -0400

    Checking in Unity project files

---
+++ SplineAnimation/Assets/SplineAnimation/Scripts/Animation/AnimateAlongSpline.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateAlongSpline : MonoBehaviour
+{
+
+    public Transform[] _electrodes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FooBob()
+    {
+        Debug.Log("Bob");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for(int i=0; i<_electrodes.Length-1; i++)
+        {
+            Gizmos.DrawLine(_electrodes[i].position, _electrodes[i + 1].position);
+        }
+    }
+}
---
+++ SplineAnimation/Assets/SplineAnimation/Scripts/Editor/SplineAnimEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AnimateAlongSpline))]
+public class SplineAnimEditor : Editor
+{
+
+    void OnEnable()
+    {
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        AnimateAlongSpline animator = (AnimateAlongSpline)target;
+        if (GUILayout.Button("Foobar"))
+        {
+            animator.FooBob();
+        }
+    }
+
+   
+}
