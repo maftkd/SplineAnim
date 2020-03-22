22c766868d5b2ecd6b2a84d74bfad5347b4618f6
Michael <michael.feldman09@gmail.com>
2020-03-22 13:28:32 -0400

    Generate points along the curve

--- SplineAnimation/Assets/SplineAnimation/Scripts/Animation/AnimateAlongSpline.cs
+++ SplineAnimation/Assets/SplineAnimation/Scripts/Animation/AnimateAlongSpline.cs
@@ -6,6 +6,8 @@ public class AnimateAlongSpline : MonoBehaviour
 {
 
     public Transform[] _electrodes;
+    public float _curveScale = 1f;
+    public int _segmentResolution = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +27,47 @@ public class AnimateAlongSpline : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+
+        Vector3[] midpoints = new Vector3[_electrodes.Length - 1];
+        Vector3[] controlPoints = new Vector3[_electrodes.Length - 1];
         for(int i=0; i<_electrodes.Length-1; i++)
         {
-            Gizmos.DrawLine(_electrodes[i].position, _electrodes[i + 1].position);
+            Gizmos.color = Color.red;
+            //Gizmos.DrawLine(_electrodes[i].position, _electrodes[i + 1].position);
+
+            midpoints[i] = Vector3.Lerp(_electrodes[i].position, _electrodes[i + 1].position, .5f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(midpoints[i], .125f);
+
+            
+            if(i < _electrodes.Length -1)
+            {
+                //calc and draw control points
+                if(i<_electrodes.Length -2)
+                    controlPoints[i] = midpoints[i] - (_electrodes[i + 2].position - _electrodes[i + 1].position - (_electrodes[i + 1].position - _electrodes[i].position)) * _curveScale;
+                else
+                    controlPoints[i] = midpoints[i];
+                Gizmos.color = Color.white;
+                Gizmos.DrawSphere(controlPoints[i], .0625f);
+
+                //calc and draw spline points
+                Gizmos.color = Color.black;
+                for (int j = 0; j < _segmentResolution; j++)
+                {
+                    float lerpVal = (float)j / (float)_segmentResolution;
+                    Vector3 pointA = Vector3.Lerp(_electrodes[i].position, controlPoints[i], lerpVal);
+                    Vector3 pointB = Vector3.Lerp(controlPoints[i], _electrodes[i + 1].position, lerpVal);
+                    Gizmos.DrawSphere(Vector3.Lerp(pointA, pointB, lerpVal), .03125f);
+                }
+            }          
+
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_electrodes[i].position, .015625f);           
+
+            
         }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(_electrodes[_electrodes.Length - 1].position, .015625f);
     }
 }
